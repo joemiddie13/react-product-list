bc91b441df0f9d1f31fd143e298cd2ec2549810f
joemiddie13 <joemiddie13@gmail.com>
2025-03-25 14:55:25 -0700

    Part 1.2 and Part 1.3

--- src/data.js
+++ src/data.js
@@ -2,3 +2,12 @@ import data from './data.json';
 
 // 1.1 Using map to extract categories
 const allCategories = data.map(item => item.category);
+
+// 1.2 Filtering unique categories
+const uniqueCategories = [...new Set(allCategories)];
+
+// 1.3 Counting products per category with reduce
+const categoryCounts = allCategories.reduce((acc, category) => {
+  acc[category] = (acc[category] || 0) + 1;
+  return acc;
+}, {});
